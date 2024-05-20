7f205c0499395d084ce2cc5f971120b40e4a2f60
Mohammad Shoaib Ansari <mdshoaibansari0307@gmail.com>
2024-05-21 00:48:59 +0530

    chore: Update home component to handle enter key press in input fields and redirect to editor page when joining room

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -2,17 +2,17 @@ import { Component } from '@angular/core';
 import { v4 as uuid } from 'uuid';
 import { FormsModule } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
-  constructor(private toastr: ToastrService) { }
+  constructor(private toastr: ToastrService, private router: Router) {}
 
   roomId: string = '';
   username: string = '';
@@ -23,4 +23,26 @@ export class HomeComponent {
     this.toastr.success('Room created successfully');
   }
 
+  joinRoom() {
+    if (this.roomId === '') {
+      this.toastr.error('Please enter a room ID');
+      return;
+    }
+    if (this.username === '') {
+      this.toastr.error('Please enter a username');
+      return;
+    }
+    this.toastr.success('Joining room');
+
+    // Redirect to the editor page
+    this.router.navigate([`/editor/${this.roomId}`], {
+      state: { username: this.username },
+    });
+  }
+
+  handleEnterKeyPress(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.joinRoom();
+    }
+  }
 }
